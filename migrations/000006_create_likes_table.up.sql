57204f538f060464d64f4a5f9c364b08f9f61a71
lya122221 <kipriyanov_2006@mail.ru>
2026-08-12 18:11:36 +0300

    Fixed creation of likes table

--- migrations/000006_create_likes_table.up.sql
+++ migrations/000006_create_likes_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS likes (
     article_id UUID NOT NULL REFERENCES articles(id) ON DELETE CASCADE,
-    user_id UUID NOT NULL REFERENCES users(id),
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     PRIMARY KEY (article_id, user_id)
 );
